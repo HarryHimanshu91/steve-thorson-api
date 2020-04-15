751c0a402a5e73e46527a0185d31ad27b5831405
Aiden Aman <aiden.aman@ithands.com>
2020-04-15 15:11:46 +0530

    fix:hide prompt menu item

--- resources/views/community/communityNavbar.blade.php
+++ resources/views/community/communityNavbar.blade.php
@@ -5,7 +5,7 @@
         <li class="custom-link {{ request()->segment('3') == 'mapdata' ? 'active' : '' }}"><a href="{{ route('admin.community.mapdata',['id'=>$id]) }}">Map Data</a></li>
         <li class="custom-link {{ request()->segment('3') == 'event' ? 'active' : '' }}"><a href="{{ route('admin.community.createevent',['id'=>$id]) }}">Events </a></li>
         <li class="custom-link {{ request()->segment('3') == 'notification' ? 'active' : '' }}"><a href="{{ route('admin.community.createnotification',['id'=>$id]) }}">Notification </a></li>
-        <li class="custom-link {{ request()->segment('3') == 'prompt' ? 'active' : '' }}"><a href="{{ route('admin.community.prompt',['id'=>$id]) }}">Prompt </a></li>
+        <!-- <li class="custom-link {{ request()->segment('3') == 'prompt' ? 'active' : '' }}"><a href="{{ route('admin.community.prompt',['id'=>$id]) }}">Prompt </a></li> -->
     </ul>
 
                    
