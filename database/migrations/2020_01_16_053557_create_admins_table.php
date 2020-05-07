98ea3a3948649e3376b25eaa5cf70b97d4d81a60
Aiden Aman <aiden.aman@ithands.com>
2020-05-07 12:49:42 +0530

    merge:merge pull requests

--- database/migrations/2020_01_16_053557_create_admins_table.php
+++ database/migrations/2020_01_16_053557_create_admins_table.php
@@ -19,6 +19,7 @@ class CreateAdminsTable extends Migration
             $table->string('email')->unique();
             $table->unsignedBigInteger('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
+            $table->integer('center_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('status')->default(0);
